96245ff275419caf06ee0cd6505143aab314673b
Fornax <burakalbayrak@live.com>
2018-01-19 13:33:22 +0300

    PHPDoc and some tiny improvements

--- config/binance.php
+++ config/binance.php
@@ -10,13 +10,13 @@ return [
      */
 
     'auth' => [
-        'key'    => env('BINANCE_KEY', ''),
-        'secret' => env('BINANCE_SECRET', ''),
+        'key'        => env('BINANCE_KEY', ''),
+        'secret'     => env('BINANCE_SECRET', '')
     ],
 
     /*
     |--------------------------------------------------------------------------
-    | Api URLS
+    | API URLs
     |--------------------------------------------------------------------------
     |
     | Binance API endpoints
@@ -26,6 +26,21 @@ return [
     'urls' => [
         'api'  => 'https://www.binance.com/api/',
         'wapi'  => 'https://www.binance.com/wapi/'
+    ],
+
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Settings
+    |--------------------------------------------------------------------------
+    |
+    | Binance API settings
+    |
+     */
+
+    'settings' => [
+        'timing' => env('BINANCE_TIMING', 5000),
+        'ssl'    => env('BINANCE_SSL_VERIFYPEER', true)
     ],
 
 ];
